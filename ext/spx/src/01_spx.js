371b0eed13b7716286a48f420cc3f76125c2c068
storycraft <storycraft@pancake.sh>
2023-07-20 17:07:00 +0900

    feat: add initial spx extension

---
+++ ext/spx/src/01_spx.js
@@ -0,0 +1,7 @@
+/*
+ * Created on Thu Jul 20 2023
+ *
+ * Copyright (c) storycraft. Licensed under the Apache Licence 2.0.
+ */
+
+
---
+++ ext/spx/src/lib.rs
@@ -0,0 +1,17 @@
+/*
+ * Created on Thu Jul 20 2023
+ *
+ * Copyright (c) storycraft. Licensed under the Apache Licence 2.0.
+ */
+
+deno_core::extension!(storyboard_spx,
+    deps = [ ],
+    ops = [],
+    esm = [ "01_spx.js" ],
+    options = {},
+    state = |state, options| {
+        
+    },
+);
+
+
